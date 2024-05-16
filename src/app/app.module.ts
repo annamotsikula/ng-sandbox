09794a43fe07f903b81fbbe75f61820e58708239
Anna <annamotsikula@gmail.com>
2024-05-16 15:44:28 +0400

    clear component for  initial app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
   ],
   imports: [
     BrowserModule,
