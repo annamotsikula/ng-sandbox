d08c039188e602b7c66506f5bd11212c2088eb0d
Anna <annamotsikula@gmail.com>
2024-06-06 20:28:46 +0400

    finish: ReactiveForms

--- src/app/survey/form.type.ts
+++ src/app/survey/form.type.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup } from "@angular/forms"
+import { FormArray, FormControl, FormGroup, UntypedFormArray } from "@angular/forms"
 
 export interface Survey {
     email?: FormControl<string | null>,
@@ -9,5 +9,14 @@ export interface Survey {
     address2?: FormControl<string | null>,
     address3?: FormControl<string | null>,
     addressCity?: FormGroup<any>,
-    contact?: FormGroup<any>
+    contact?: FormGroup<any>,
+    checkUser?: FormControl<boolean | null>,
+    skills?: UntypedFormArray,
+    requirements?: FormArray<FormGroup<RequirementGroup>>
+
+}
+
+export interface RequirementGroup {
+    addr1: FormControl<string | null>,
+    addr2: FormControl<string | null>
 }
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { passwordValidator } from '../helpers/validators/password.validator';
-import { Survey } from './form.type';
+import { RequirementGroup, Survey } from './form.type';
 
 @Component({
   selector: 'app-survey',
@@ -10,7 +10,7 @@ import { Survey } from './form.type';
 })
 export class SurveyComponent {
   surveyForm = new FormGroup<Survey>({
-    email: new FormControl(null, [Validators.email, Validators.pattern('^[a-zA-Z]+$')]),
+    email: new FormControl('test@test.com', [Validators.email, Validators.pattern('^[a-zA-Z]+$')]),
     password: new FormControl(null, passwordValidator),
     fullName: new FormControl(null, [Validators.maxLength(10), Validators.required, Validators.minLength(3)]),
     age: new FormControl(null, [Validators.max(80)]),
@@ -21,16 +21,32 @@ export class SurveyComponent {
       state: new FormControl(),
       zip: new FormControl()
     }),
+    checkUser: new FormControl(false, [Validators.requiredTrue]),
+    skills: new FormArray([
+      new FormControl()
+    ]),
+    requirements: new FormArray([
+      new FormGroup({
+        addr1: new FormControl(),
+        addr2: new FormControl(),
+      })
+    ])
   });
 
   ngOnInit() {
     console.log(this.surveyForm)
+    this.surveyForm.controls.email?.disable();
+    
+
+
   }
 
   submitForm() {
-    // if(this.surveyForm.valid) {
-      console.log(this.surveyForm)
-    // }
+    if(this.surveyForm.valid) {
+      console.log(this.surveyForm.getRawValue());
+
+    }
+
   }
 
   changeName() {
@@ -48,14 +64,35 @@ export class SurveyComponent {
 
   removeControl(name: keyof Survey) {
     // const control = this.surveyForm.get(name) as AbstractControl
-    this.surveyForm.removeControl(name) 
+    this.surveyForm.removeControl(name); 
   }
   getControlName(control: string) {
     return this.surveyForm.get(control) as FormControl
   }
 
-  // get passwordError() {
-  //   // 
-  //   // return
-  // }
+  addSkills() {
+    const skillsArr = this.surveyForm.controls.skills;
+    const newControl = new FormControl("", [Validators.required]);
+    skillsArr?.push(newControl);
+
+    console.log(skillsArr)
+  }
+  addNewRequirement() {
+    const arr = this.surveyForm.controls.requirements;
+    const newRequirement = new FormGroup<RequirementGroup>({
+      addr1: new FormControl(null, [Validators.required]),
+      addr2: new FormControl(null, [Validators.required]),
+    });
+    arr?.push(newRequirement)
+  }
+
+  deleteArr(index: number, arrName: 'skills' | 'requirements') {
+    const arr = this.surveyForm.controls[arrName];
+    arr?.removeAt(index)
+  }
+  get skillsArr() {
+    
+   return this.surveyForm.controls.skills
+  }
+
 }
